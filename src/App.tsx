b5f1002b4ad1dcbb6c086930bc6b0dd935698731
Galuh Wasesa <surat.sesha@gmail.com>
2025-05-12 21:03:35 +0700

    chore : create smart schedule react app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
+const tasks = [
+  {
+    id: 1,
+    title: "Go to Garden",
+    completed: true,
+    date: new Date("2025-05-12 07:00"),
+  },
+  {
+    id: 2,
+    title: "Breakfast",
+    completed: false,
+    date: new Date("2025-05-12 10:00"),
+  },
+];
+
 export function App() {
   return (
     <div>
       <h1>Smart Schedule</h1>
+
+      <ul>
+        {tasks.map((task) => {
+          return (
+            <li key={task.id}>
+              <h2>{task.title}</h2>
+              <p>{task.completed ? "Completed" : "Not Completed"}</p>
+              <p>{task.date.toLocaleString()}</p>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
