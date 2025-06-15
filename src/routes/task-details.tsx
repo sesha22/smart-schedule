f11d589bca00b04cd97efa4752cd75b4ef953da8
M Haidar Hanif <git@mhaidarhanif.com>
2025-06-15 14:59:20 +0700

    docs: todo to render task details, and create label input components

--- src/routes/task-details.tsx
+++ src/routes/task-details.tsx
@@ -4,9 +4,12 @@ export function TaskDetailsRoute() {
   const params = useParams();
   const taskId = params.taskId;
 
+  // TODO: Get the task details
+
   return (
     <div>
       <h1>Task Details: {taskId}</h1>
+      {/* TODO: Render task details in here */}
     </div>
   );
 }
